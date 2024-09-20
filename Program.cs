b9de2e267eeb33ccef502c12e4e1a975a232bbaa
KissAdam <kissa@kkszki.hu>
2024-09-20 13:08:28 +0200

    The result is ready

--- Program.cs
+++ Program.cs
@@ -8,14 +8,38 @@ namespace terulet
 {
     internal class Program
     {
-        static void test1()
+        static void terulet()
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Téglalap területének számítása");
+
+            int a;
+            int b;
+            int T;
+
+            do
+            {
+                Console.WriteLine("Kérem az 'a' oldal értékét. (0 > kell lennie)");
+                a = Convert.ToInt32(Console.ReadLine());
+            } while (a <= 0);
+
+            do
+            {
+                Console.WriteLine("Kérem az 'b' oldal értékét. (0 > kell lennie)");
+                b = Convert.ToInt32(Console.ReadLine());
+            } while (b <= 0);
+
+            T = a * b;
+
+
+            Console.WriteLine($"A téglalap területe {T}");
+
+            Console.ReadKey();
+
         }
         
         static void Main(string[] args)
         {
-            test1 ();
+            terulet();
         }
     }
 }
